89db86866aa59c4a4fbb2cb0e175ad265e8de065
UhyunInha <167306922+UhyunInha@users.noreply.github.com>
2024-05-15 07:06:30 +0900

    users db 생성

--- sql.py
+++ sql.py
@@ -1,6 +1,7 @@
 import sqlite3
 
 conn = sqlite3.connect('database.db')
+cur = conn.cursor()
 
 conn.execute(
 '''
@@ -18,4 +19,15 @@ create table tennis (match text, time text, details text)
 '''
 )
 
+cur.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT NOT NULL UNIQUE,
+        email TEXT NOT NULL,
+        password TEXT NOT NULL
+    )
+''')
+
+cur.execute("INSERT INTO users (username, email, password) VALUES (?, ?, ?)", ('유현', 'hello@inha.edu', 'world'))
+conn.commit()
 conn.close()
