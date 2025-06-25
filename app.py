19ef6da96f0e19e8806cfe0d5cc97431e18b5764
Yoo Hyun Kim <uuuhyun.kim@gmail.com>
2025-06-25 21:31:25 +0900

    Update app.py

--- app.py
+++ app.py
@@ -794,6 +794,8 @@ def load_vs_list():
     return vs_list
 
 
+import os
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 8000))  # Azure는 PORT 환경변수를 자동 설정
+    app.run(host='0.0.0.0', port=port, debug=True)
